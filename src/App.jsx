e1e8fd6f937160d659f110bf634e3a8cf9579111
avavion <avavion@gmail.com>
2023-01-13 14:56:02 +0300

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
       <Header />
-      <Header />
-      <Header />
-      <Header />
-      <Header />
-      <Header />
-      <Header />
-      <Header />
-
       <h1>Hello</h1>
       <p>Paragraph</p>
+      <Footer />
     </div>
   );
 }
