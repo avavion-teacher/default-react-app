7bba3e55fa4be2fef2e078d71bad102138058cab
avavion <avavion@gmail.com>
2023-01-13 14:29:35 +0300

    Init react app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+
+      <h1>Hello</h1>
+      <p>Paragraph</p>
+    </div>
+  );
+}
+
+export default App;
